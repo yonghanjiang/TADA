11b3dbccc16697b3530f8c742f25f5d4b179b0a1
yonghanjiang <1144764005@qq.com>
2026-08-16 05:52:55 +0000

    update

--- train_tada.sh
+++ train_tada.sh
@@ -6,5 +6,5 @@ python -u main.py \
     --config-file test/test_odinw13 \
     --model-config-file groundingdino/config/GroundingDINO_SwinT_OGC_dt_tada.py \
     --model-checkpoint-path ./weights/groundingdino_swint_ogc.pth \
-    --output-dir output/ours_seed0_random30 \
+    --output-dir output/tada \
     --seed 0 --num-gpus 1
